672a414a75821d53c20a84114d3703f1394fb667
Ali-Vijdan <alivijdan55@gmail.com>
2022-12-27 18:57:41 +0500

    task complete

---
+++ src/tree.js
@@ -0,0 +1,24 @@
+export default {
+  module: "project-title",
+  id: "root-0",
+  children: [
+    {
+      id: "root-1",
+      module: "public.html",
+      collapsed: true,
+    },
+    {
+      id: "root-2",
+      module: "src",
+      children: [
+        {
+          id: "4",
+          module: "app.js",
+          leaf: true
+        },
+       
+      ]
+    },
+   
+  ]
+};
